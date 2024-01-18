5bf17e4727a4ac0f05aef1666df5a1aa8dc310b3
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-01-18 12:03:25 +0100

    Complete exercise 05. Correct some smelly control for no-numbers parameters. Explore the sytax of array-destructuring.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let result = 0
+
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max) ) return "ERROR"
+
+    if (min > max){
+        [min, max] = [max, min]
+    }
+
+    for (min; min <= max; min++){
+        result += min;
+    }
+    return result
+
 
 };
 
