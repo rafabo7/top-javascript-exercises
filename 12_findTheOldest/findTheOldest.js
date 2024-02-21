fde03ebfa47def10f509a48aaef383d272e12d7d
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-21 21:54:35 +0100

    Solve problem 12_findTheOldest without a headache. The soltution provided if more verbose than mine. Although mine sorts the original array and then don't use it, not so practical after all.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.map((item) => {
+        if(!item.yearOfDeath){
+            item.lived = (new Date().getFullYear()) - item.yearOfBirth
+        }else{
+            item.lived = item.yearOfDeath - item.yearOfBirth
+        }
+    })
 
+    let sorted = people.sort((a,b) => a.lived > b.lived ? 1 : -1)
+    return sorted[sorted.length-1]
 };
 
 // Do not edit below this line
