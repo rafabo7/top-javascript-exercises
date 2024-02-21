5b613a3a6c5a1359d053a4ed6e77e6fac9985786
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-21 21:20:25 +0100

    Solve the problem 10_fibpnacci Added a fibonacci array perse, for working better and better readability.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //Check for negative numbers
+    if (num < 0) return 'OOPS'
+    //Check the type of the argument, convert to number if necessary
+    if (typeof num === 'string'){
+        num = Number(num)
+    }
+    //Lets Fibonacci
+    let a = 0
+    let b = 1
+    let c = 0
+    //A little hardcoded but readable
+    const fib = [0, 1]
+    for (let i = 0; i < num; i++){
+        c = a + b
+        fib.push(c)
+        a = b
+        b = c
+    }
+    console.log(fib)
+    return fib[num]
 
 };
 
