4988f3c85fe79ef8a01854a192f16f97e665f4de
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-21 12:07:19 +0100

    Solve problema 09_palindromes. With great effort Apparently Javascript does not like to compare arrays

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    return cleanString == cleanString.split("").reverse().join("")
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
